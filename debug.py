e44ac800f46c9ebfee710a62bc245bc6c5554548
icenshy <icenshy@gmail.com>
2022-11-28 19:33:44 +0700

    first commit

---
+++ debug.py
@@ -0,0 +1,11 @@
+"""Test Something"""
+import pygame
+
+pygame.init()
+
+font = pygame.font.Font(None, 30)
+
+def debug(info, y=10, x=10):
+    display_surface = pygame.display.get_surface()
+    text_surf = font.render(str(info), True, 'Black')
+    display_surface.blit(text_surf, (x,y))
---
+++ main.py
@@ -0,0 +1,30 @@
+"""Pelda Project"""
+
+import pygame, sys
+from settings import *
+from debug import *
+
+
+class Game:
+    def __init__(self):
+        
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Pelda Project")
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill('Pink')
+            debug("Hello")
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
