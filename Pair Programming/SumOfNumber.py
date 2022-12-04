5f705b6e8e9f1ef974dfa724e2210966a4c3ca2c
icenshy <icenshy@gmail.com>
2022-12-04 17:51:00 +0700

    test

--- Pair Programming/SumOfNumber.py
+++
@@ -1,5 +0,0 @@
-my_card = ''
-for _ in range(51):
-    my_card += '\''+input() + '\''+','
-
-print(my_card+'\'KD\'')
--- Pair Programming/WeightStation.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-hash_dict = dict()
-
-for i in range(100000):
-    hash_dict.update(
-        {hashlib.sha512(("%05d" % i).encode()).hexdigest().upper(): i})
-
-print(hash_dict[input()])
